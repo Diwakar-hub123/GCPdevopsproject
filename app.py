ece5d9898cc03b44a128fbed134e562d2dbaa3e2
Diwakar-hub123 <83933859+Diwakar-hub123@users.noreply.github.com>
2023-12-12 21:29:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, Simple Flask application I am done with deployment'
+  return 'Hello, Simple Flask application checking with the deployment'
